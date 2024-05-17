51f9321e242126aa0220767091d80a4caac00f32
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-17 11:58:33 -0400

    Create CatFall.sh

---
+++ CatFall.sh
@@ -0,0 +1,6 @@
+cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+echo -e "Made By Hydradedgaming"
+echo -e "its installing rn gimme a min"
+curl "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/GetDaTheme.sh"
+echo -e "ight lil bro this is done have fun"
+echo -e "btw dm me with theme ideas"
