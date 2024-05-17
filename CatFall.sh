b94a16c9cee361f628f5ce09bc5038dffabd44e1
hydradedgaming <167041346+hydradedgaming@users.noreply.github.com>
2024-05-17 12:22:15 -0400

    CatFall.sh

--- CatFall.sh
+++ CatFall.sh
@@ -1,6 +1,6 @@
 cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
 echo -e "Made By Hydradedgaming"
 echo -e "its installing rn gimme a min"
-curl "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/GetDaTheme.sh"
+curl "curl "https://raw.githubusercontent.com/hydradedgaming/MacSploit_Themes/main/GetDaTheme.js" -o "./theme-tomorrow_night.js"
 echo -e "ight lil bro this is done have fun"
 echo -e "btw dm me with theme ideas"
